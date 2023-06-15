9366a0dd387d384016bc9af62ec10d17232deadb
Lucas Giovani de Paula Salgado <45075913+LucaSalgado@users.noreply.github.com>
2023-06-14 23:20:26 -0300

    navBar totalmente responsiva incompleta

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
+  menuVisivel = true;
+  isMobile = false;
 
+  @HostListener('window:resize', ['$event'])
+
+  onWindowResize(event: any) {
+    // Verifica se a tela saiu do modo mobile (largura maior que 768 pixels)
+    if (event.target.innerWidth >= 768) {
+      this.menuVisivel = true; // Exibe os links
+      this.isMobile = false;
+    } else if (event.target.innerWidth < 768 && !this.isMobile) {
+      this.menuVisivel = false;
+      this.isMobile = true;
+    }
+  }
+
+  dropdownMenu(): void {
+    this.menuVisivel = !this.menuVisivel;
+  }
 }
